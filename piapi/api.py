b83f9de765bd360ced247bb2ffbc6baa14f37978
Yuday <87972280+Limolu@users.noreply.github.com>
2024-10-26 10:15:04 +0800

    Add files via upload

---
+++ piapi/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+# 設定 GPIO 模式和腳位
+GPIO.setmode(GPIO.BCM)
+GPIO_PIN = 16
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+
+# GPIO 開啟
+@app.route('/gpio_on', methods=['GET'])
+def gpio_on():
+    GPIO.output(GPIO_PIN, GPIO.HIGH)
+    return jsonify({"status": "GPIO is ON"}), 200
+
+# GPIO 關閉
+@app.route('/gpio_off', methods=['GET'])
+def gpio_off():
+    GPIO.output(GPIO_PIN, GPIO.LOW)
+    return jsonify({"status": "GPIO is OFF"}), 200
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=80)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
